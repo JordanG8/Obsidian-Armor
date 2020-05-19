b52a5c1bf04e3fa7aff1c7de262aeaf72ab97462
JordanG8 <34008592+JordanG8@users.noreply.github.com>
2020-05-19 15:17:36 +0300

    Update Main.java

--- src/main/java/me/jordang8/obsidianarmor/Main.java
+++ src/main/java/me/jordang8/obsidianarmor/Main.java
@@ -2,6 +2,7 @@ package me.jordang8.obsidianarmor;
 
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -15,55 +16,95 @@ import me.mrCookieSlime.Slimefun.cscorelib2.config.Config;
 import me.mrCookieSlime.Slimefun.cscorelib2.item.CustomItem;
 
 public class Main extends JavaPlugin implements SlimefunAddon {
-
+// testing
     @Override
     public void onEnable() {
-        // Read something from your config.yml
         Config config = new Config(this);
 
         if (config.getBoolean("options.auto-update")) {
-            // You could start an Auto-Updater for example
         }
-
-        // Slimefun4 also already comes with a bundled version of bStats
-        // You can use bStats to collect usage data about your plugin
-        // More info: https://bstats.org/getting-started
-        // Set bStatsId to the id of your plugin
         int bStatsId = -1;
         new Metrics(this, bStatsId);
 
-        // Create a new Category
-        // This Category will use this ItemStack
         ItemStack categoryItem = new CustomItem(Material.OBSIDIAN, "&8Obsidian-Armor", "", "&a> Click to open");
 
-        // Give your Category a unique id.
-        NamespacedKey categoryId = new NamespacedKey(this, "&8Obsidian-Armor");
+        NamespacedKey categoryId = new NamespacedKey(this, "OBSIDIAN_ARMOR");
         Category category = new Category(categoryId, categoryItem);
-
-        // Create a new Slimefun ItemStack
-        // This class has many constructors, it is important that you give each item a unique id.
-        SlimefunItemStack slimefunItem = new SlimefunItemStack("OBSIDIAN_CHESTPLATE", Material.DIAMOND_CHESTPLATE, "&8Obsidian chestplate", "&c+20% Coolness");
-
-        // The Recipe is an ItemStack Array with a length of 9.
-        // It represents a Shaped Recipe in a 3x3 crafting grid
-        // The machine in which this recipe is crafted in is specified further down
-        ItemStack[] recipe = { new ItemStack(Material.OBSIDIAN), null, new ItemStack(Material.OBSIDIAN), new ItemStack(Material.OBSIDIAN), new ItemStack(Material.OBSIDIAN), new ItemStack(Material.OBSIDIAN), new ItemStack(Material.OBSIDIAN), new ItemStack(Material.OBSIDIAN), new ItemStack(Material.OBSIDIAN) };
-
-        // Now you just have to register the item
-        // RecipeType.ENHANCED_CRAFTING_TABLE refers to the machine in which this item is crafted in.
-        // Recipy Types from Slimefun itself will automatically add the recipe to that machine
-        SlimefunItem item = new SlimefunItem(category, slimefunItem, RecipeType.ENHANCED_CRAFTING_TABLE, recipe);
+        
+        
+        SlimefunItemStack obs_alloy = new SlimefunItemStack("OBSIDIAN_ALLOY", Material.DIAMOND, "&8Obsidian alloy", "&c use this item to craft obsidian Armor");
+        ItemStack[] recipe = {  new ItemStack(Material.DIAMOND), new ItemStack(Material.DIAMOND),	new ItemStack(Material.DIAMOND),
+        						new ItemStack(Material.DIAMOND), new ItemStack(Material.OBSIDIAN), 	new ItemStack(Material.DIAMOND),
+        						new ItemStack(Material.DIAMOND), new ItemStack(Material.DIAMOND), 	new ItemStack(Material.DIAMOND) };
+        SlimefunItem item = new SlimefunItem(category, obs_alloy, RecipeType.ENHANCED_CRAFTING_TABLE, recipe);
         item.register(this);
+        
+        SlimefunItemStack obsHelmet = new SlimefunItemStack("OBSIDIAN_HELMET", Material.DIAMOND_HELMET, "&8Obsidian helmet", "&c Grants you full fire and lava protection" );
+        ItemStack[] recipeForHelmet = {			obs_alloy, obs_alloy, obs_alloy,
+        											obs_alloy, 	null, 	  obs_alloy,
+        											null, 		null, 		null };
+		SlimefunItem obs_helmet = new SlimefunItem(category, obsHelmet, RecipeType.ENHANCED_CRAFTING_TABLE, recipeForHelmet);
+        final ItemStack isHelmet = obsHelmet;
+        
+        isHelmet.addUnsafeEnchantment(Enchantment.DURABILITY, 3);
+        isHelmet.addUnsafeEnchantment(Enchantment.PROTECTION_FIRE, 100);
+        isHelmet.addUnsafeEnchantment(Enchantment.PROTECTION_EXPLOSIONS, 100);
+        isHelmet.addUnsafeEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 4);
+       
+        obs_helmet.register(this);
+
+        SlimefunItemStack obsChestplate = new SlimefunItemStack("OBSIDIAN_CHESTPLATE", Material.DIAMOND_CHESTPLATE, "&8Obsidian chestplate", "&c Grants you full fire and lava protection" );
+        ItemStack[] recipeForChestplate = {		obs_alloy, 		null, obs_alloy,
+        										obs_alloy, obs_alloy, obs_alloy,
+        										obs_alloy, obs_alloy, obs_alloy };
+		SlimefunItem obs_chestplate = new SlimefunItem(category, obsChestplate, RecipeType.ENHANCED_CRAFTING_TABLE, recipeForChestplate);
+        final ItemStack is = obsChestplate;
+        
+        is.addEnchantment(Enchantment.DURABILITY, 3);
+        is.addUnsafeEnchantment(Enchantment.PROTECTION_FIRE, 100);
+        is.addUnsafeEnchantment(Enchantment.PROTECTION_EXPLOSIONS, 100);
+        is.addUnsafeEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 4);
+       
+        obs_chestplate.register(this);
+        
+        
+        
+        SlimefunItemStack obsLeggings = new SlimefunItemStack("OBSIDIAN_LEGGINGS", Material.DIAMOND_LEGGINGS, "&8Obsidian leggings", "&c Grants you full fire and lava protection" );
+        ItemStack[] recipeForLeggings = {		obs_alloy, obs_alloy, obs_alloy,
+        										obs_alloy, 		null, obs_alloy,
+        										obs_alloy, 		null, obs_alloy };
+		SlimefunItem obs_leggings = new SlimefunItem(category, obsLeggings, RecipeType.ENHANCED_CRAFTING_TABLE, recipeForLeggings);
+        final ItemStack isLeggings = obsChestplate;
+        
+        isLeggings.addEnchantment(Enchantment.DURABILITY, 3);
+        isLeggings.addUnsafeEnchantment(Enchantment.PROTECTION_FIRE, 100);
+        isLeggings.addUnsafeEnchantment(Enchantment.PROTECTION_EXPLOSIONS, 100);
+        isLeggings.addUnsafeEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 4);
+       
+        obs_leggings.register(this);
+        
+        
+        SlimefunItemStack obsBoots = new SlimefunItemStack("OBSIDIAN_BOOTS", Material.DIAMOND_BOOTS, "&8Obsidian boots", "&c Grants you full fire and lava protection" );
+        ItemStack[] recipeForBoots = {				null,		null, 	null, 
+        										obs_alloy, 		null, obs_alloy,
+        										obs_alloy, 		null, obs_alloy };
+		SlimefunItem obs_boots = new SlimefunItem(category, obsBoots, RecipeType.ENHANCED_CRAFTING_TABLE, recipeForBoots);
+        final ItemStack isBoots = obsChestplate;
+        
+        isBoots.addEnchantment(Enchantment.DURABILITY, 3);
+        isBoots.addUnsafeEnchantment(Enchantment.PROTECTION_FIRE, 100);
+        isBoots.addUnsafeEnchantment(Enchantment.PROTECTION_EXPLOSIONS, 100);
+        isBoots.addUnsafeEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 4);
+       
+        obs_boots.register(this);
     }
 
     @Override
     public void onDisable() {
-        // Logic for disabling the plugin...
     }
 
     @Override
     public String getBugTrackerURL() {
-        // You can return a link to your Bug Tracker instead of null here
         return null;
     }
 
